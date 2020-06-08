66093e1094e3e345688575599d12f05b446890d8
fengzhongjie <15850512966@163.com>
2020-06-08 23:01:22 +0800

    update

--- test_manger/settings.py
+++ test_manger/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'test_manger.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testmanger',
+        'USER': 'root',
+        'PASSWORD': '168168',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
